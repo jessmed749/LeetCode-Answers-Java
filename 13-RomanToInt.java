55fb0e210bd659c079622306f90f3c3be2aec410
Jessica Medina <jessica.lizmedina@gmail.com>
2025-02-04 00:04:16 -0800

    Comments for 13

--- 13-RomanToInt.java
+++ 13-RomanToInt.java
@@ -1,14 +1,17 @@
 
-
 class Solution {
     public int romanToInt(String s) {
         int total = 0;
         int result_current = 0;
 
+        // Accessing the string from end to start, because how do you read roman numbers?
         for(int i = s.length() - 1; i >= 0; i--){
+            // Check current and next value
             int current = asign_value(s.charAt(i));
             int next_value = (i > 0) ? asign_value(s.charAt(i - 1)) : 0;
             
+            // If current > next, then we have a case like 4, 9, 40, 90,... whatever
+            // We need to subtract the next value from the current value and add it to the total
             if(current > next_value){
                 result_current = current - next_value;
                 total += result_current;
@@ -21,6 +24,7 @@ class Solution {
         return total;
     }
 
+    // Method to assign value to roman numbers
     public int asign_value(char roman){
         switch (roman){
             case 'I': return 1;
@@ -34,10 +38,10 @@ class Solution {
         }
     }
 
+    // Test cases to make debugging ez
     public static void testCases() {
         Solution solution = new Solution();
 
-        // Test cases
         System.out.println("Test MCMXCIV: " + (solution.romanToInt("MCMXCIV") == 1994));
         System.out.println("Test III: " + (solution.romanToInt("III") == 3));
         System.out.println("Test IV: " + (solution.romanToInt("IV") == 4));
